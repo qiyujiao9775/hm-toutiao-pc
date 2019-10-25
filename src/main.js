91435f036d9c6c4428d19c454749d1407b8d6a39
jiao~ <jiao~>
2019-10-25 20:20:46 +0800

    使用vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
+// 导入文件的时候，默认导入的是目录下的索引文件（index.js index.vue index.json）。
+// import router from '/src/router/index.js'
+import router from '@/router'
+Vue.use(ElementUI)
+Vue.config.productionTip = false
 new Vue({
+  // el: '#app',
+  router,
   render: h => h(App)
 }).$mount('#app')
